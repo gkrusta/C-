3d0f45303deef6d80311b286842e5d2627333e6b
Gabriela <krustagabriela@gmail.com>
2024-01-04 17:30:35 +0100

    on deposit

--- module00/ex02/Account.cpp
+++ module00/ex02/Account.cpp
@@ -6,40 +6,66 @@
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 11:39:32 by gkrusta           #+#    #+#             */
-/*   Updated: 2024/01/04 12:00:01 by gkrusta          ###   ########.fr       */
+/*   Updated: 2024/01/04 17:30:28 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
-Account::Account(void) {
-	_accountIndex = 0;
-	_amount = 0;
-	_nbDeposits = 0;
-	_nbWithdrawals = 0;
+static int		_nbAccounts = 0;
+static int		_totalAmount = 0;
+static int		_totalNbDeposits = 0;
+static int		_totalNbWithdrawals = 0;
+
+t::Account(int initial_deposit) {
+	_displayTimestamp();
+	_accountIndex = _nbAccounts;
+	_amount = initial_deposit;
+	_accountIndex = _nbAccounts;
+	std::cout << "index: " << getNbAccounts() << ";amount:" << checkAmount() << ";created" << std::endl;
+	_nbAccounts++;
 }
 
-int	Account::getNbAccounts(void) {
+int	t::getNbAccounts(void) {
 	return (_nbAccounts);
 }
 
-int	Account::getTotalAmount(void) {
+int	t::getTotalAmount(void) {
 	return (_totalAmount);
 }
 
-int	Account::getNbDeposits(void) {
+int	t::getNbDeposits(void) {
 	return (_totalNbDeposits);
 }
 
-int	Account::getNbWithdrawals(void) {
+int	t::getNbWithdrawals(void) {
 	return (_totalNbWithdrawals);
 }
 
-void	_displayTimestamp( void ) {
+int	t::checkAmount(void) const {
+	return (_amount);
+}
+
+void	makeDeposit( int deposit ) {
+	_totalNbDeposits++;
+	
+}
+//here :)
+bool	makeWithdrawal( int withdrawal ) {
+	if (withdrawal > _amount.checkAmount)
+		return false;
+	else {
+		
+		_totalNbWithdrawals++;
+		return true;
+	}
+}
+
+void	t::displayAccountsInfos( void ) {
 	_displayTimestamp();
 	std::cout << "index:" << _nbAccounts.getNbAccounts() << ";" <<
 }
 
-void	Account::_displayTimestamp( void ) {
+void	t::_displayTimestamp( void ) {
 	
 }
--- module00/ex02/Account.hpp
+++ module00/ex02/Account.hpp
@@ -12,6 +12,10 @@
 #ifndef __ACCOUNT_H__
 #define __ACCOUNT_H__
 
+#include <iostream>
+#include <iomanip>
+#include <string>
+
 // ************************************************************************** //
 //                               Account Class                                //
 // ************************************************************************** //
@@ -52,7 +56,7 @@ private:
 	int				_nbDeposits;
 	int				_nbWithdrawals;
 
-	Account( void );
+	Account( void ); //never using this private method
 
 };
 
