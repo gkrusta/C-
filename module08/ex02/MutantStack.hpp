bc583837cfd4bbb4f6d20c779d15ed1003161d44
Gabis <krustagabriela@gmail.com>
2024-04-02 14:13:31 +0300

    tests with std::list added

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -8,9 +8,9 @@
 template <typename T>
 class   MutantStack : public std::stack<T> {
 	public:
-		MutantStack() : std::stack<T>();
-		~MutantStack();
-		MutantStack(const MutantStack<T>& other) : std::stack<T>(other);
+		MutantStack() : std::stack<T>() {};
+		~MutantStack() {};
+		MutantStack(const MutantStack<T>& other) : std::stack<T>(other) {};
 		MutantStack<T>& operator=(const MutantStack<T>& other) {
 			if (this != &other) {
 				this->c = other.c;
@@ -22,7 +22,9 @@ class   MutantStack : public std::stack<T> {
 	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
 	iterator	begin() { return this->c.begin(); };
-	const_iterator	begin() { return this->c.begin(); };
+	const_iterator	begin() const { return this->c.begin(); };
+	iterator	end() { return this->c.end(); };
+	const_iterator end() const { return this->c.end(); };
 };
 
 #endif
--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -2,33 +2,60 @@
 
 int main()
 {
-	MutantStack<int> mstack;
+	{
+		std::cout << "--- testing MutantStack ---" << std::endl;
+		MutantStack<int> mstack;
+		std::cout << "adding {1, 3, 5} to the stack..." << std::endl;
+		mstack.push(1);
+		mstack.push(3);
+		mstack.push(5);
+		std::cout << "size: " << mstack.size() << std::endl << "mstack.top() -> "<< mstack.top() << std::endl;
+		std::cout << "removing last added element with mstack.pop()... " << std::endl;
+		mstack.pop();
+		std::cout << "size: " << mstack.size() << std::endl;
 
-	mstack.push(5);
-	mstack.push(17);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
 
-	std::cout << mstack.top() << std::endl;
-
-	mstack.pop();
-
-	std::cout << mstack.size() << std::endl;
-	
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-
-	++it;
-	--it;
-	
-	while (it != ite) {
-		std::cout << *it << std::endl;
 		++it;
+		--it;
+		std::cout << "iterating over the stack..." << std::endl;
+		while (it != ite) {
+			std::cout << *it << "  ";
+			++it;
+		}
+
+		std::cout << "\n\n--- copying MutantStack ---" << std::endl;
+		std::stack<int> s(mstack);
+		std::cout << "stack conetnt (in reverse order):" << std::endl;
+		while (!s.empty()) {
+			int	elem = s.top();
+			s.pop();
+			std::cout << elem << "  ";
+		}
+	}	
+	{	
+		std::cout << "\n\n--- replacing the MutantStack with a std::list ---" << std::endl;
+		std::list<int>	myList;
+		std::cout << "adding {1, 3, 5} to the list..." << std::endl;
+		myList.push_back(1);
+		myList.push_back(3);
+		myList.push_back(5);
+		std::cout << "size: " << myList.size() << std::endl << "myList.back() -> "<< myList.back() << std::endl;
+		std::cout << "removing last added element with myList.pop_back()... " << std::endl;
+		myList.pop_back();
+		std::cout << "size: " << myList.size() << std::endl;
+
+		std::list<int>::iterator it = myList.begin();
+		std::list<int>::iterator ite = myList.end();
+
+		++it;
+		--it;
+		std::cout << "iterating over the list..." << std::endl;
+		while (it != ite) {
+			std::cout << *it << "  ";
+			++it;
+		}
 	}
-	std::stack<int> s(mstack);
 	return 0;
 }
