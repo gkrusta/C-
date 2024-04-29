e1ca9e4f7afa66aee5171af9f98aeabc5b456d5c
Gabriela <krustagabriela@gmail.com>
2024-04-29 16:57:07 +0200

    refactored

--- module09/ex01/main.cpp
+++ module09/ex01/main.cpp
@@ -2,8 +2,8 @@
 
 int	main(int argc, char **argv) {
 	if (argc != 2) {
-		std::cerr << "Error: Pass an expresion for example like this \"3 5 + 7 2 - *\"" << std::endl;
-		return 1;
+		std::cerr << "Error: Pass an expresion for example like this: \"3 5 + 7 2 - *\"" << std::endl;
+		return (1);
 	}
 	if (argc == 2) {
 		RPN	Rpn;
